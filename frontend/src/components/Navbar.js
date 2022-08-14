96981a9ac9808fad82f6e66bbebfe8034881a1fa
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-14 13:16:01 -0500

    updated NavBar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,12 +5,12 @@ function Navbar () {
  return (
    <React.Fragment>
      {/* //copied from bootstrap https://getbootstrap.com/docs/4.0/components/navbar/*/}
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-       <a class="navbar-brand" href="#">
+     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+       <a className="navbar-brand" href="#">
          RAJH Frames
        </a>
        <button
-        class="navbar-toggler"
+        className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarNav"
@@ -18,32 +18,32 @@ function Navbar () {
           aria-expanded="true"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-       <div class="collapse navbar-collapse" id="navbarNav">
-         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-           <li class="nav-item active">
-             <a class="nav-link" href="#">
-               Home <span class="sr-only">(current)</span>
+       <div className="collapse navbar-collapse" id="navbarNav">
+         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+           <li className="nav-item active">
+             <a className="nav-link" href="#">
+               Home <span className="sr-only">(current)</span>
              </a>
            </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
+           <li className="nav-item">
+             <a className="nav-link" href="#">
                Men's
              </a>
            </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
+           <li className="nav-item">
+             <a className="nav-link" href="#">
                Women's
              </a>
            </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
+           <li className="nav-item">
+             <a className="nav-link" href="#">
                Children's
              </a>
            </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
+           <li className="nav-item">
+             <a className="nav-link" href="#">
                Sunglasses
              </a>
            </li>
