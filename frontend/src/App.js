b03244fa69d6dd85aea082e23ec80eabd1b6aa7d
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-14 14:06:20 -0500

    footer and product holder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Product from './components/Product'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+ 
 
 function App() {
   return (
-   <Navbar />
-  );
+    <div>
+      <Navbar />
+      <Product />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,19 +1,25 @@
-// import React from 'react'
-// import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
-// function Footer{
-//     return(
+function Footer() {
+  return (
+    <div className="card text-center">
+      <div className="card-header-dark bg-dark text-white">Featured</div>
+      <div className="card-body">
+        <h5 className="card-title">Rate the Product</h5>
+        <p className="card-text">
+          We would love to hear from you please leave your feedback by clicking
+          the button below!
+        </p>
+        <a href="#" className="btn btn-primary">
+          Rating
+        </a>
+      </div>
+      <div className="card-footer-dark bg-dark text-white">
+        Check back for new brands weekly
+      </div>
+    </div>
+  )
+}
 
-//         <React.Fragment>
-
-//         </React.Fragment>
-
-//     )
-// }
-
-
-
-
-
-
-export default Footer;
+export default Footer
