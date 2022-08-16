93c3d8f112a38b355c4b4639a2520a14e74150e7
estesthebestes <esteshunter22@gmail.com>
2022-08-16 10:00:32 -0500

    Working on creating the general structure of the customer_controller file in the back end. I am getting this done ahead of time so that during class the group can give me thoughts about how to proceed

--- backend/controllers/customer_controller.js
+++ backend/controllers/customer_controller.js
@@ -1,13 +1,19 @@
 //DEPENDENCIES
+const customer = require('express').Router();
+const db = require('../models');
+const { Customer, Inventory, Review, Transaction } = db;
+const { Op } = require('sequelize');
 
 //FIND ALL CUSTOMER IDS
-
+customer.get('/', async (req, res) => {});
 //FIND A SPECIFIC CUSTOMER ID
+customer.get('/:customer_id', async (req, res) => {});
 
 //CREATE A CUSTOMER ID
+customer.post('/', async (req, res) => {});
 
 //UPDATE A CUSTOMER ID
-
+customer.put('/:id', async (req, res) => {});
 // DELETE A CUSTOMER ID
 
 //EXPORT A CUSTOMER ID
