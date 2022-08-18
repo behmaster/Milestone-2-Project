0d87006455d9f0795053a6c9a6026c7d0316d8f7
Robbie Behm <behmaster@gmail.com>
2022-08-18 18:11:32 -0500

    progress on productview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Product from './components/ProductGrid'
+import Product from './views/Product'
 import Rating from './components/Rating'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   return (
     <div>
-      <Navbar />
+      <Product />
     <h1>Welcome Page</h1>
     </div>
   )
--- frontend/src/components/ProductView.js
+++ frontend/src/components/ProductView.js
@@ -1,24 +1,36 @@
-import ProductInfo from "./components/ProductInfo";
-import ProductReview from "./components/ProductReview";
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import samplePhoto from '../assets/sample.webp'
+import Product from './ProductGrid';
 
-function ProductView() {
 
-
-  return( 
-  <div>
-
-    //Product Photo
-    //Name, Rating, Price, Colors, 'Add to Cart Button'
-    <h1>{product_name}</h1>
-
-    <h2>colors</h2>
-    {colors} 
-    <button>Add To Cart</button>
-
-      <ProductInfo />
-      <ProductReview />
-  </div>
+const ProductView = () => {
+  return (
+    <Container>
+        <Row>
+        <Col sm={8}>
+        <img src={samplePhoto} alt="Sample Photo" />
+        <img
+       src="https://media.istockphoto.com/photos/nerd-glasses-with-clipping-paths-picture-id125416806?k=20&m=125416806&s=612x612&w=0&h=5debF0r2pbWcHHxNl9EOZwQ5O52JlKUIpy2pW0t8rfA="
+       alt="glasses"
+       width="500"
+       height="600"
+     ></img>
+        </Col>
+        <Col sm={4}>
+            {Product.title}
+            {Product.rating}
+            {Product.price}
+            {Product.colors}
+            <Button>Add To Cart</Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
-export default ProductView;
+export default ProductView
