51b20a51799545547a41cfea8d5b6deb51c54cc6
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-20 13:16:52 -0500

    first route working!

--- backend/controllers/inventory_controller.js
+++ backend/controllers/inventory_controller.js
@@ -1,23 +1,38 @@
-//DEPENDENCIES
+// //DEPENDENCIES
 const inventory = require('express').Router();
 const db = require('../models');
 const { Inventory, Customer, Review, Transaction } = db;
 const { Op } = require('sequelize');
 
-//GET INVENTORY IDs
-inventory.get('/', async (req, res) => {});
 
-//FIND A SPECIFIC ITEM
-inventory.get('/:customer_id', async (req, res) => {});
+inventory.get('/', async (req, res) => {
+  try {
+    const foundInventory = await Inventory.findAll({
+      where: {
+        model: {
+          [Op.like]: `%${req.query.model ? req.query.model : ''}%`,
+        },
+      },
+    })
+    res.status(200).json(foundCustomers)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+// //GET INVENTORY IDs
+// inventory.get('/', async (req, res) => {});
 
-//CREATE AN ITEM IN THE INVENTORY
-inventory.post('/', async (req, res) => {});
+// //FIND A SPECIFIC ITEM
+// inventory.get('/:id', async (req, res) => {});
 
-//UPDATE AN ITEM IN THE INVENTORY
-inventory.put('/:id', async (req, res) => {});
+// //CREATE AN ITEM IN THE INVENTORY
+// inventory.post('/', async (req, res) => {});
 
-// DELETE AN ITEM
-inventory.delete('/:id', async (req, res) => {});
+// //UPDATE AN ITEM IN THE INVENTORY
+// inventory.put('/:id', async (req, res) => {});
 
-//EXPORT AN ITEM
+// // DELETE AN ITEM
+// inventory.delete('/:id', async (req, res) => {});
+
+// //EXPORT AN ITEM
 module.exports = inventory;
--- backend/models/review.js
+++ backend/models/review.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class review extends Model {
+  class Review extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,15 +13,37 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  review.init({
-    rating_id: DataTypes.INTEGER,
-    customer_id: DataTypes.INTEGER,
-    product_id: DataTypes.INTEGER,
-    rating_result: DataTypes.INTEGER,
-    comment: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'review',
-  });
-  return review;
+  Review.init(
+    {
+      rating_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      customer_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      rating_result: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      comment: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+    
+    },
+    {
+      sequelize,
+      modelName: 'Review',
+      tableName: 'reviews',
+      timestamps: false
+    }
+  )
+  return Review;
 };
