0e9b922bb13dd20781ce2fa44d2ef73cac5175c5
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-21 10:10:54 -0500

    updated controllers and header

--- backend/controllers/inventory_controller.js
+++ backend/controllers/inventory_controller.js
@@ -1,24 +1,60 @@
 // //DEPENDENCIES
-const inventory = require('express').Router();
+const inventories = require('express').Router();
 const db = require('../models');
-const { Inventory, Customer, Review, Transaction } = db;
+const { Inventory} = db;
 const { Op } = require('sequelize');
 
 
-inventory.get('/', async (req, res) => {
+inventories.get('/', async (req, res) => {
   try {
-    const foundInventory = await Inventory.findAll({
+    const foundInventories = await Inventory.findAll({
       where: {
-        model: {
-          [Op.like]: `%${req.query.model ? req.query.model : ''}%`,
+        category: {
+          [Op.like]: `%${req.query.category ? req.query.category : ''}%`,
         },
       },
     })
-    res.status(200).json(foundCustomers)
+    res.status(200).json(foundInventories)
   } catch (error) {
     res.status(500).json(error)
   }
 })
+inventories.get('/:model', async (req, res) => {
+  try {
+    const foundInventory = await Inventory.findOne({
+      where: { model: req.params.model
+         
+      },
+    })
+    res.status(200).json(foundInventory)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// inventories.get('/:category', async (req, res) => {
+//   try {
+//     const foundInventories = await Inventory.findAll({
+//       where: { category: req.params.category },
+//     })
+//     res.status(200).json(foundInventories)
+//   } catch (error) {
+//     res.status(500).json(error)
+//   }
+// })
+
+// inventories.get('/:mens', async (req, res) => {
+//   try {
+//     const foundInventory = await Inventory.findAll({
+//       where: { category: req.params.category
+         
+//       },
+//     })
+//     res.status(200).json(foundInventory)
+//   } catch (error) {
+//     res.status(500).json(error)
+//   }
+// })
 // //GET INVENTORY IDs
 // inventory.get('/', async (req, res) => {});
 
@@ -35,4 +71,4 @@ inventory.get('/', async (req, res) => {
 // inventory.delete('/:id', async (req, res) => {});
 
 // //EXPORT AN ITEM
-module.exports = inventory;
+module.exports = inventories;
